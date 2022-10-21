71f017171c17c34065d7a91dd2af640429fa3c9f
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2022-10-21 10:14:41 +0100

    add unit tests for file finding

---
+++ internal/dir/files_test.go
@@ -0,0 +1,56 @@
+package dir_test
+
+import (
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/thaffenden/pkb/internal/dir"
+)
+
+func TestGetAllFilesInDirectory(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		inputDir    string
+		assertError require.ErrorAssertionFunc
+		expected    []string
+	}{
+		"ReturnsAllFilesInDirectory": {
+			inputDir:    "no-ignores",
+			assertError: require.NoError,
+			expected: []string{
+				"testdata/no-ignores/one",
+				"testdata/no-ignores/two",
+				"testdata/no-ignores/three",
+			},
+		},
+		"DoesNotReturnFilesIngnoredDirectory": {
+			inputDir:    "ignores",
+			assertError: require.NoError,
+			expected: []string{
+				"testdata/ignores/foo",
+				"testdata/ignores/bar",
+			},
+		},
+		"ReturnsEmptySliceForEmptyDirectory": {
+			inputDir:    "empty",
+			assertError: require.NoError,
+			expected:    []string{},
+		},
+	}
+
+	for name, testCase := range testCases {
+		tc := testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := dir.GetAllFilesInDirectory(filepath.Join("testdata", tc.inputDir))
+			tc.assertError(t, err)
+
+			reflect.DeepEqual(tc.expected, actual)
+		})
+	}
+}
