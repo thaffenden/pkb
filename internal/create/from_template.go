84bc6a835baca7048ad7e8305cd990fd475c4ec2
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2022-10-23 16:19:02 +0100

    use viper's config file rather than explict defined one

--- internal/create/from_template.go
+++ internal/create/from_template.go
@@ -12,6 +12,7 @@ import (
 	"text/template"
 	"time"
 
+	"github.com/spf13/viper"
 	"github.com/thaffenden/pkb/internal/config"
 	"github.com/thaffenden/pkb/internal/date"
 	"github.com/thaffenden/pkb/internal/dir"
@@ -65,7 +66,7 @@ func (t TemplateRenderer) CreateAndSaveFile() (string, error) {
 	}
 
 	templateFile := filepath.Clean(
-		filepath.Join(t.Config.TemplateDir, t.SelectedTemplate.File),
+		filepath.Join(filepath.Dir(viper.ConfigFileUsed()), t.SelectedTemplate.File),
 	)
 
 	contents, err := ioutil.ReadFile(templateFile)
