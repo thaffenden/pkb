6f2c287cc9669c0c03f71b0cf61d19240ae7fa1f
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2022-10-23 15:28:02 +0100

    add example of viper config to see if that solves the issue

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,13 @@ package cmd
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/thaffenden/pkb/internal/config"
+	"github.com/thaffenden/pkb/internal/flags"
 )
 
 // Version is the CLI version set via linker flags at build time.
@@ -27,7 +31,26 @@ func Execute(conf config.Config) error {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
 	rootCmd.AddCommand(CreateNew())
 	rootCmd.AddCommand(CreateEdit())
 	rootCmd.AddCommand(CreateCopy())
+	rootCmd.PersistentFlags().StringVar(&flags.ConfigFile, "config", "", "config file if not held at default location")
+	viper.BindPFlag("config", rootCmd.PersistentFlags().Lookup("config"))
+}
+
+func initConfig() {
+	if flags.ConfigFile == "" {
+		viper.SetConfigName("config")
+		viper.SetConfigType("json")
+		viper.AddConfigPath("$XDG_CONFIG_DIR/pkb/")
+		viper.AddConfigPath("$HOME/.config/pkb/")
+	} else {
+		viper.SetConfigFile(flags.ConfigFile)
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("error trying to read config file: %s", err)
+		os.Exit(1)
+	}
 }
---
+++ internal/flags/vars.go
@@ -0,0 +1,6 @@
+// Package flags contains interactions with the global vars used as CLI flags
+// for the different commands.
+package flags
+
+// ConfigFile is the variable for the `--config` CLI flag.
+var ConfigFile string
